0ce45121a450d21b58f741f4a74a4fdb407a444d
Tim Arnold <jtim.arnold@gmail.com>
2014-06-17 13:09:33 -0400

    enhanced packaging script to do almost everything for ctan

--- packaging_scripts/packages.py
+++ packaging_scripts/packages.py
@@ -1,6 +1,11 @@
 import os
+import shutil
+import shlex
+import subprocess
+import time
+
 '''
-Creates statrep.dtx and longfigure.dtx in subdirectories of current dir.
+Creates statrep.dtx and longfigure.dtx in appropriate subdirectories.
 Files present:
     statrep.wrapper
     longfigure.wrapper
@@ -10,6 +15,40 @@ statrep.wrapper is read and longfigure.inc is inserted to create statrep.dtx
 longfigure.wrapper is read and longfigure.inc is inserted to create longfigure.dtx
 
 '''
+ROOT = os.path.normpath(os.path.join(os.getcwd(), '..'))
+
+EXTRAS = os.path.join(ROOT, 'extras')
+
+#
+CTAN = os.path.join(ROOT, 'ctan')
+TESTING = os.path.join(ROOT, 'testing')
+
+
+def run_pdf(name):
+    def pdf():
+        cmd = str('pdflatex %s.dtx' % name)
+        p = subprocess.Popen(shlex.split(cmd), cwd=cwd)
+        p.wait()
+    def idx():
+        cmd = str('makeindex -s gind.ist %s' % name)
+        p = subprocess.Popen(shlex.split(cmd), cwd=cwd)
+        p.wait()
+
+    cwd = os.path.join(TESTING, name)
+    pdf();pdf()
+    idx();pdf()
+
+def start_clean():
+    for d in [CTAN, TESTING]:
+        if os.path.isdir(d):
+            for f in os.listdir(d):
+                fname = os.path.join(d, f)
+                if os.path.isfile(fname):
+                    os.unlink(fname)
+            shutil.rmtree(d)
+            time.sleep(2)
+        os.mkdir(d)
+
 
 def readfile(name):
     with open(name) as f:
@@ -25,14 +64,39 @@ def insert(wrapper, content):
 
     return newlines
 
-def write_dtx(name, lines):
-    if not os.path.isdir(name):
-        os.mkdir(name)
-    fname = '%s/%s.dtx' % (name, name)
+def write_dtx(package, lines):
+    dname = os.path.join(TESTING, package)
+    fname = os.path.join(TESTING, package, '%s.dtx' % package)
+    if not os.path.isdir(dname):
+        os.mkdir(dname)
+
     with open(fname, 'wb') as f:
         f.writelines(lines)
 
+def copyfiles(package):
+    tgtdir = os.path.join(CTAN, package)
+    if not os.path.isdir(tgtdir):
+        os.mkdir(tgtdir)
+
+    src =  os.path.join(TESTING, package, 'README.')
+    tgt = os.path.join(tgtdir, 'README')
+    shutil.copy(src, tgt)
+
+    for ext in ['dtx', 'ins', 'pdf']:
+        src = os.path.join(TESTING, package, '%s.%s' % (package, ext))
+        tgt = os.path.join(tgtdir, '%s.%s' % (package, ext))
+        shutil.copy(src, tgt)
+
+    if package == 'statrep':
+        for dname in [CTAN, TESTING]:
+            tgtdir = os.path.join(dname, package)
+            for name in os.listdir(EXTRAS):
+                src = os.path.join(EXTRAS, name)
+                tgt = os.path.join(tgtdir, name)
+                shutil.copy(src, tgt)
+
 def main():
+    start_clean()
     statrep = readfile('statrep.wrapper')
     longfigure = readfile('longfigure.wrapper')
     inc = readfile('longfigure.inc')
@@ -40,6 +104,11 @@ def main():
     write_dtx('statrep', insert(statrep, inc))
     write_dtx('longfigure', insert(longfigure, inc))
 
+    run_pdf('statrep')
+    run_pdf('longfigure')
+
+    copyfiles('statrep')
+    copyfiles('longfigure')
 
 
 if __name__ == '__main__':
